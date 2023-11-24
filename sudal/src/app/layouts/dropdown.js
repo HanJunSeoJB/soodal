e1dbd9034faa618ca256aacea1fc7c1e3c766fa6
HanJunSeoJB <hjs123@jbnu.ac.kr>
2023-11-25 01:28:34 +0900

    feat: 드롭메뉴 기능 구현 완료
    드롭 메뉴 기능 구현 완료

--- sudal/src/app/layouts/dropdown.js
+++ sudal/src/app/layouts/dropdown.js
@@ -7,22 +7,21 @@ const DropdownMenu = () => {
 
   // 마우스 이벤트 핸들러
   const handleMouseEnter = () => {
-    alert("hi");
     setIsOpen(true);
   }
   const handleMouseLeave = () => setIsOpen(false);
 
   return (
-    <li className='group mr-32 relative' onMouseEnter={handleMouseEnter}>
-      <Link href="/materials" onMouseEnter={handleMouseEnter}>학습자료</Link>
-      <div className='w-full h-80 flex justify-center bg-skyblue'>
-        <div 
-          className={`w-4/6 h-270 rounded-b-lg bg-white shadow-inner ${isOpen ? 'block' : 'hidden'}`}
-        >
-          {/* 드롭다운 메뉴 내용 */}
+    <div className='w-auto'>
+      <li className='group mr-32 relative' onMouseLeave={handleMouseLeave}>
+        <Link href="/materials" onMouseOver={handleMouseEnter}>학습자료</Link>
+      </li>
+      <div className='w-full h-80 absolute flex justify-center bg-skyblue left-0'>
+          <div className={`w-4/6 h-270 rounded-b-lg bg-white shadow-inner ${isOpen ? 'block' : 'hidden'}`}>
+            {/* 드롭다운 메뉴 내용 */}
+          </div>
         </div>
-      </div>
-    </li>
+  </div>
   );
 };
 
