a7d0e9294a89edd0541d7555452d5df27b49dc07
Haneungyu <105436435+EungyuHan@users.noreply.github.com>
2023-11-25 15:43:34 +0900

    feat: HTML Escape

--- sudal/src/app/detail/[id]/page.js
+++ sudal/src/app/detail/[id]/page.js
@@ -1,4 +1,6 @@
 // 'use client'
+
+import { sanitize, isSupported } from "isomorphic-dompurify";
 import { connectDB } from "../../../../util/database"
 import { ObjectId } from "mongodb"
 
@@ -8,10 +10,10 @@ export default async function Detail(props){
     let result = await db.collection('post').findOne({ _id : new ObjectId(props.params.id)})
 
     return(
-        <div>
+        <div >
             <h4>상세페이지</h4>
-            <h4>{result.title}</h4>
-            <p>{result.content}</p>
+            <h4 dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(result.title, { ALLOWED_TAGS: ['p', 'b', 'i', 'u', 'img', 'a'] })}} />
+            <p dangerouslySetInnerHTML={{ __html:  DOMPurify.sanitize(result.content, { ALLOWED_TAGS: ['p', 'b', 'i', 'u', 'img', 'a'] })}} />
         </div>
     )
 }
