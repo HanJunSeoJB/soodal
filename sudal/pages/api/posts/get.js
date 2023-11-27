64534769d9c9ad4f33f60162fa89b49d0267274c
jayjayjayjayjayjayjayjayjay <qoralswo29@gmail.com>
2023-11-27 10:10:59 +0900

    feat: 게시판 get 기능 추가

--- sudal/pages/api/posts/get.js
+++ sudal/pages/api/posts/get.js
@@ -1,10 +1,16 @@
 import { connectDB } from "../../../util/database";
+import { ObjectId } from 'mongodb';
 
 export default async function handler(req, res) {
     if(req.method != 'GET') { return res.status(400).json('잘못된 접근입니다.') }
-    
-    const db = (await connectDB).db('posts')
-    const result = await db.collection('post').find().toArray()
 
+    const db = (await connectDB).db('posts')
+    let boardId = (await db.collection('board').findOne({boardName: req.query.board}))._id
+
+    console.log(boardId)
+
+    const result = await db.collection('post').find({_id: new ObjectId(boardId)}).toArray();
+
+    
     res.status(200).json(result)
 }
