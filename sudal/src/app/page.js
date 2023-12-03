78b6ac0bc279d49f78c332caa56c970dfa288d3d
HanJunSeoJB <hjs123@jbnu.ac.kr>
2023-12-03 18:45:26 +0900

    feat: 인기자료 탭 및 글자 생성
    인기자료 탭 생성 및 검색 글자 생성

--- sudal/src/app/page.js
+++ sudal/src/app/page.js
@@ -4,6 +4,11 @@ export default function Home() {
   return (
     <div>
       <Image src={Banner} alt='배너' className="w-full h-auto"></Image>
+      <p className="mt-14 flex justify-center font-['Gsanslight'] text-2xl">어떤 걸 찾고있수달?</p>
+      {/*인기자료 탭 */}
+      <div>
+
+      </div>
     </div>
   )
 }
