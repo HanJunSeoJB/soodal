ada744beb986c2ceabb88a67b4411edd1d93a44f
백민재 <baegminjae@baegminjaeui-MacBookPro.local>
2024-01-07 05:06:06 +0900

    feat: 회원가입 png 파일 추가 및 auth 오류 수정

---
+++ sudal/pages/api/auth/signup.js
@@ -0,0 +1,15 @@
+import { connectDB } from "@/util/database";
+import bcrypt from "bcrypt";
+
+export default async function handler(요청, 응답) {
+  if (요청.method === "POST") {
+      const hash = await bcrypt.hash(요청.body.password, 10);
+      요청.body.password = hash;
+
+      let db = (await connectDB).db('users');
+      await db.collection('user').insertOne(요청.body);
+      응답.status(200).json('성공');
+
+      console.log(요청.body);
+  }
+}; 
