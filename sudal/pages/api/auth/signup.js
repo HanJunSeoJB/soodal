7e817f9d4c7f47969dc0b78cb8e233e4a2fb49fe
백민재 <baegminjae@baegminjaeui-MacBookPro.local>
2024-01-07 21:37:51 +0900

    -

--- sudal/pages/api/auth/signup.js
+++ sudal/pages/api/auth/signup.js
@@ -5,7 +5,7 @@ export default async function handler(요청, 응답) {
   if (요청.method === "POST") {
     const db = (await connectDB).db('users');
     const { username, password, email, name, birthdate } = 요청.body; // 요청에서 필드 추출
-
+    //ㅇㅇ
     const document = {
       _id: new ObjectId(), // ObjectId를 생성하여 사용합니다.
       username: username,
@@ -17,5 +17,5 @@ export default async function handler(요청, 응답) {
 
     await db.collection('user').insertOne(document);
     응답.status(200).json('성공');
-  }
+  } 
 };
