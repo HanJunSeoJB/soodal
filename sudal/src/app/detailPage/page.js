82023df95ba1f738c3ce5ce05db30499382dddff
Daeuning <happyjde3@gmail.com>
2024-01-08 21:47:10 +0900

    feat: starRating 컴포넌트 구현 완료

--- sudal/src/app/detailPage/page.js
+++ sudal/src/app/detailPage/page.js
@@ -1,8 +1,44 @@
+'use client'
+import StarRating from "./starRating";
+
 export default function DetailPage() {
   return (
-      // 전체 viewport margin 설정
-      <div>
-          Hello world!
+      //children페이지 전체 크기 설정
+      <div className='flex flex-col w-[1000px]'>
+        {/*네비게이션 바*/}
+        <div className='flex flex-row justify-between ml-[36px] mt-[41px]'>
+          <div className='flex flex-row'>
+            <p className="text-[23px] text-[#303030] font-['Gsans']"> &lt; </p>
+            <p className="text-[23px] text-[#303030] font-['GsansBold'] ml-[18px]">
+            [2015 개정] 내신1등급 도전 단계별 변형문제 </p>
+          </div>
+          <div className='flex'>
+            <p className="mt-[3px] text-[13px] text-[#303030] font-['Gsanslight']">Home &gt; 고등내신 &gt; 과목선택 &gt; 자료검색</p>
+          </div>
+        </div>
+
+        {/*자료 세부사항 및 방문버튼*/}
+        <div className='flex flex-row justify-between ml-[70px] mt-[39px]'>
+          {/*별점*/}
+          <div className='flex flex-col'>
+            <p className="text-[20px] text-[#444444] font-['PretendardMedium']">지이다 영어교실</p>
+            <div className='flex flex-row'>
+              {/*별*/}
+              <div>
+                <StarRating/>
+              </div>
+              {/*평점*/}
+              <div>
+
+              </div>
+            </div>
+          </div>
+
+          {/*버튼들*/}
+          <div>
+
+          </div>
+        </div>
       </div>
   )
 }
---
+++ sudal/src/app/detailPage/starRating.js
@@ -0,0 +1,57 @@
+'use client'
+import {useEffect} from 'react';
+
+export default function StarRating() {
+  //rating변수에 DB에서 가져온 평점이 들어가면 됩니다.
+  const rating = 3.4;
+  const ratingMask = 5.0 - rating;
+  const starSize = 16, maxStar = 5, gutter = 4;
+  const maskSize = starSize * ratingMask + (Math.floor(ratingMask) * gutter);
+
+  useEffect(() => {
+    // useEffect를 사용하여 컴포넌트가 렌더링된 후에 코드를 실행합니다.
+    const maskElement = document.getElementById('mask');
+    if (maskElement) {
+      // 'maskSize'를 기반으로 'mask' 엘리먼트의 너비를 동적으로 설정합니다.
+      maskElement.style.width = `${maskSize}px`;
+    }
+  }, [maskSize]);
+
+  return (
+    //rating-wrap
+    <div className='flex mt-[6px]'>
+      {/*rating*/}
+      <div className='flex relative items-center'>
+        {/*overlay*/}
+        <div id='mask' className='absolute mix-blend-color top-0 right-0 bottom-0 bg-white opacity-100 z-10 opacity: unset'>
+        </div>
+        {/*star*/}
+        <div className='w-[16px] h-[15px]'>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16.359" height="15.558" viewBox="0 0 16.359 15.558">
+            <path id="패스_937" data-name="패스 937" d="M8.179,0,5.651,5.122,0,5.943,4.089,9.93l-.965,5.629L8.179,12.9l5.055,2.658L12.269,9.93l4.09-3.987-5.652-.821L8.179,0Z" transform="translate(0 0)" fill="#fc0"/>
+          </svg>
+        </div>
+        <div className='w-[16px] h-[15px] ml-[4px]'>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16.359" height="15.558" viewBox="0 0 16.359 15.558">
+            <path id="패스_937" data-name="패스 937" d="M8.179,0,5.651,5.122,0,5.943,4.089,9.93l-.965,5.629L8.179,12.9l5.055,2.658L12.269,9.93l4.09-3.987-5.652-.821L8.179,0Z" transform="translate(0 0)" fill="#fc0"/>
+          </svg>
+        </div>
+        <div className='w-[16px] h-[15px] ml-[4px]'>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16.359" height="15.558" viewBox="0 0 16.359 15.558">
+            <path id="패스_937" data-name="패스 937" d="M8.179,0,5.651,5.122,0,5.943,4.089,9.93l-.965,5.629L8.179,12.9l5.055,2.658L12.269,9.93l4.09-3.987-5.652-.821L8.179,0Z" transform="translate(0 0)" fill="#fc0"/>
+          </svg>
+        </div>
+        <div className='w-[16px] h-[15px] ml-[4px]'>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16.359" height="15.558" viewBox="0 0 16.359 15.558">
+            <path id="패스_937" data-name="패스 937" d="M8.179,0,5.651,5.122,0,5.943,4.089,9.93l-.965,5.629L8.179,12.9l5.055,2.658L12.269,9.93l4.09-3.987-5.652-.821L8.179,0Z" transform="translate(0 0)" fill="#fc0"/>
+          </svg>
+        </div>
+        <div className='w-[16px] h-[15px] ml-[4px]'>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16.359" height="15.558" viewBox="0 0 16.359 15.558">
+            <path id="패스_937" data-name="패스 937" d="M8.179,0,5.651,5.122,0,5.943,4.089,9.93l-.965,5.629L8.179,12.9l5.055,2.658L12.269,9.93l4.09-3.987-5.652-.821L8.179,0Z" transform="translate(0 0)" fill="#fc0"/>
+          </svg>
+        </div>
+      </div>
+    </div>
+  )
+}
