7e0ac916a109587af9839927c0912b7c7cc8bcbc
백민재 <baegminjae@baegminjaeui-MacBookPro.local>
2024-01-19 23:24:51 +0900

    feat : auth session 저장 오류 해결

--- sudal/src/app/login/layout.js
+++ sudal/src/app/login/layout.js
@@ -1,11 +1,11 @@
 
 'use client'
-
 import { SessionProvider } from "next-auth/react"
 
-export default function Layout({ children }){
+export default function Layout({ children, session }){
+
   return (
-    <SessionProvider>
+    <SessionProvider session={session}>
       {children}
     </SessionProvider>
   )
