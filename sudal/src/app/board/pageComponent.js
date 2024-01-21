b57ee12350ed424a0ad3fd08305a84afcf5465f4
Haneungyu <105436435+EungyuHan@users.noreply.github.com>
2024-01-21 22:13:00 +0900

    feat: 페이지 리스트 정렬제외 구현 완료

--- sudal/src/app/board/pageComponent.js
+++ sudal/src/app/board/pageComponent.js
@@ -1,29 +1,50 @@
 'use client'
-import Link from 'next/link'
 
 import { useRouter, useSearchParams, usePathname } from 'next/navigation';
+import Link from 'next/link'
 
 function SortAt({board}){
+  const pathname = usePathname(); // 현재 경로를 불러오기 위한 hook
+  const router = useRouter(); // 페이지 새로고침을 위한 hook
+  const params = useSearchParams(); // url 쿼리를 가져오기 위한 hook
+
+  const queryParams = {};
+  params.forEach((value, key) => {
+    queryParams[key] = value;
+  });
+
+
   const sortOptions = (()=>{
     switch(board){
       case 'qna':
-        return ['조회수', '나도 궁금 순', '답변 많은 순', '답변 적은 순'];
+        return {'조회수':'views', '나도 궁금 순':'nado', '답변 많은 순':'descend_answer', '답변 적은 순':'ascend_answer'};
       default:
-        return ['인기순', '조회수', '추천순', '스크랩순'];
+        return {'인기순':'popular', '조회수':'views', '추천순':'recommend', '스크랩순':'scrap'};
     }
   })();
-  return(
+  const handleSort = (value) => {
+    // sort를 갱신
+    queryParams.sort = value;
+    // queryParams의 key-value를 queryString으로 변환
+    const queryString = Object.entries(queryParams)
+      .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
+      .join('&');
+    // 쿼리를 변경하고 페이지를 새로고침
+    router.push(`${pathname}?${queryString}`);
+  }
+
+  return (
     <div className="flex flex-row text-base">
-      {sortOptions.map((option, index) => (
+      {Object.entries(sortOptions).map(([key, value], index, array) => (
         <span key={index} className="flex flex-row font-['Gsans']">
-          <button className="">
-            {option}
+          <button className="" onClick={() => handleSort(value)}>
+            {key}
           </button>
-          <p className="mx-1 text-gray-400">{index < sortOptions.length - 1 ? '|' : ''}</p>
+          <p className="mx-1 text-gray-400">{index < array.length - 1 ? '|' : ''}</p>
         </span>
       ))}
     </div>
-  )
+  );
 }
 
 function List({posts, board}) {
@@ -33,14 +54,13 @@ function List({posts, board}) {
         {posts.map((post, key) => (
           <Question
             key={key}
+            id={post._id}
             title={post.title}
             author={post.author}
             date={post.date}
             nado={post.nado}
             view={post.view}
             comment={post.comment}
-            id={post._id}
-            board = {board}
           />
         ))}
       </div>
@@ -51,6 +71,7 @@ function List({posts, board}) {
       {posts.map((post, key) => (
         <Post
           key={key}
+          id={post._id}
           title={post.title}
           author={post.author}
           date={post.date}
@@ -58,22 +79,18 @@ function List({posts, board}) {
           scrap={post.scrap}
           view={post.view}
           comment={post.comment}
-          id={post._id}
-          board = {board}
         />
       ))}
     </div>
   );
 }
 
-function Question({title, author, date, nado, view, comment, board}) {
+function Question({id, title, author, date, nado, view, comment}) {
   return(
     <div className="flex flex-row items-center font-['PretendardMedium'] h-10 border-b">
       <div className="flex flex-row items-start w-1/2 ml-2">
         <Popular />
-        <button className="text-[18px] truncate mr-[3px]">
-          {title}
-        </button>
+        <Link prefetch={false} className="text-[18px] truncate mr-[3px]" href={`/detail/${id}`}>{title}</Link>
         <Comment comment={comment} />
       </div>
       <button className="text-center w-28">{author}</button>
@@ -84,21 +101,19 @@ function Question({title, author, date, nado, view, comment, board}) {
   )
 }
 
-function Post({title, author, date, recommend, scrap, view, comment, id, board}) {
+function Post({id, title, author, date, recommend, scrap, view, comment}) {
   return (
-    <div>
-      <div className="flex flex-row items-center font-['PretendardMedium'] h-10 border-b">
-        <div className="flex flex-row items-start w-1/2">
-          <Popular />
-          <Link className="text-[18px] truncate mr-[3px]" href={`/detail/${id}?board=${board}`}>{title}</Link>
-          <Comment comment={comment} />
-        </div>
-        <button className="text-center w-28 text-center text-[16px] truncate">{author}</button>
-        <p className="text-center w-32 text-center text-[16px] truncate">{date}</p>
-        <p className="text-center w-20 text-center text-[16px] truncate">{recommend}</p>
-        <p className="text-center w-20 text-center text-[16px] truncate">{scrap}</p>
-        <p className="text-center w-20 text-center text-[16px] truncate">{view}</p>
+    <div className="flex flex-row items-center font-['PretendardMedium'] h-10 border-b">
+      <div className="flex flex-row items-start w-1/2 ml-2">
+        <Popular />
+        <Link prefetch={false} className="text-[18px] truncate mr-[3px]" href={`/detail/${id}`}>{title}</Link>
+        <Comment comment={comment} />
       </div>
+      <button className="text-center w-28 text-center text-[16px] truncate">{author}</button>
+      <p className="text-center w-32 text-center text-[16px] truncate">{date}</p>
+      <p className="text-center w-20 text-center text-[16px] truncate">{recommend}</p>
+      <p className="text-center w-20 text-center text-[16px] truncate">{scrap}</p>
+      <p className="text-center w-20 text-center text-[16px] truncate">{view}</p>
     </div>
   );
 }
@@ -150,8 +165,7 @@ function PageSize(){
   )
 }
 
-function Pagenation(){
-  let total=251
+function Pagenation({total}){
   const PAGE_GROUP_SIZE =10
   const pathname = usePathname(); // 현재 경로를 불러오기 위한 hook
   const router = useRouter(); // 페이지 새로고침을 위한 hook
@@ -185,7 +199,6 @@ function Pagenation(){
       break;
     }
   }
-  console.log(parseFloat(total)*parseFloat(pageSize));
 
   return (
     <div className="flex flex-row justify-center items-center font-['Gsans'] text-[16px] text-gray gap-x-[34px]">
