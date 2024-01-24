2ae3394f87866bb1042833bf20437fdf4373d630
Daeuning <happyjde3@gmail.com>
2024-01-24 10:29:15 +0900

    feat: 세부페이지 모달 디자인 수정

--- sudal/src/app/detailPage/commentComponents/selectTag.js
+++ sudal/src/app/detailPage/commentComponents/selectTag.js
@@ -5,10 +5,25 @@ export default function SelectTag({ clickModal }) {
     clickModal();
   };
 
+  const handleInnerClick = (e) => {
+    // 내부 div를 클릭해도 부모의 클릭 이벤트가 실행되지 않도록 함
+    e.stopPropagation();
+  };
+
   return (
-      <div className="absolute top-0 left-0 w-[200px] h-[100px] z-100 rounded-[19px] border border-[#D5D5D5] py-[33px] px-[40px] shadow-md">
-        <p>hello world!</p>
-        <button onClick={handleCloseModal}>X</button>
+    <div className='fixed flex justify-center items-center top-0 left-0 bottom-0 right-0 bg-black bg-opacity-50'>
+      <div className='fixed w-1/2 h-auto rounded-[19px] bg-white border border-[#D5D5D5] py-[50px] px-[25px] shadow-md' onClick={handleInnerClick}>
+        <button className="absolute top-[21px] right-[32px] text-[20px] text-[#9C9C9C] font-['Gsans']"
+        onClick={handleCloseModal}>X</button>
+        <div className='flex flex-col'>
+          <div className='flex items-end'>
+            <p className="text-[21px] text-[#303030] font-['GsansBold']">어떤 점이 좋았나요?</p> &nbsp;
+            <p className="text-[17px] text-[#303030] font-['Gsans'] mb-[1px]">(1개 ~ 5개)</p>
+          </div>
+          <p className="text-[16px] text-[#303030] font-['PretendardMedium']">이 자료에 어울리는 키워드를 골라주세요</p>
+
+        </div>
       </div>
+    </div>
   );
 }
