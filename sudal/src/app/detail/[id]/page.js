48f531d4719cb2127de66d1102fd4923c7540f6a
HanJunSeoJB <hjs123@jbnu.ac.kr>
2024-01-27 18:44:20 +0900

    refactor: 게시글 확인 페이지 최적화
    게시글 확인 페이지 성능 최적화

--- sudal/src/app/detail/[id]/page.js
+++ sudal/src/app/detail/[id]/page.js
@@ -8,24 +8,27 @@ import ListButton from "../../layouts/ListButton"
 
 
 export default async function Detail(props) {
-    fetch('http://localhost:3000/api/posts/getDetail?id='+props.params.id)
-    .then(res => res.json().then(data => {
-        let dateObj = new Date(data.updatedAt)
-        let year = dateObj.getFullYear();
-        let month = dateObj.getMonth()+1; // 월은 0부터 시작하므로 1을 더해줍니다.
-        let day = dateObj.getDate();
-        let title = data.title;
-        let author = data.author;
-        let view = data.view;
-    })
-    )
+    async function getDetail(id) {
+        const result = await fetch(
+           ` http://localhost:3000/api/posts/getDetail?id=${id}`,
+        {cache: 'no-cache'},
+        );
+        const data = await result.json();
+        return data;
+    }
+        
     const boardNames = {
         free: '자유게시판',
         qna: 'Q&A',
         info: '정보게시판'
     };
     let boardName = boardNames[props.searchParams.board];
-    
+    const detail = await getDetail(props.params.id);
+    const dateObj = new Date(detail.updatedAt);
+    const year = dateObj.getFullYear();
+    const month = dateObj.getMonth() + 1;
+    const day = dateObj.getDate();
+
     return (
         <div className="flex flex-row">
             <DetailLayout board = {boardName}>
@@ -38,17 +41,17 @@ export default async function Detail(props) {
                         <h1 className="font-['Gsans'] text-base">{boardName}</h1>
                         <p className="font-['Gsanslight text-xs']">Home {">"} {boardName}</p>
                     </div>
-                    <p className="mt-1 font-['GsansBold'] text-2xl">{title}</p>
+                    <p className="mt-1 font-['GsansBold'] text-2xl">{detail.title}</p>
                     {/* 세부정보 */}
                     <div className="flex justify-between">
                         <div className="mt-2 flex flex-row text-grey items-center">
-                            <p>{author}</p>
+                            <p>{detail.author}</p>
                             {/* 막대 바 */}
                             <div className="border-0.25 h-3.5 ml-3 border-darkgrey"></div>
                             <p className="ml-3">{year}. {month}. {day}</p>
                             {/* 막대 바 */}
                             <div className="border-0.25 h-3.5 ml-3 border-darkgrey"></div>
-                            <p className="ml-3">조회 {result.view}</p>
+                            <p className="ml-3">조회 {detail.view}</p>
                         </div>
                         <ListButton _id={props.params.id} board={props.searchParams.board}/>
                     </div>
@@ -56,16 +59,16 @@ export default async function Detail(props) {
                     {/*긴 막대 바 */}
                     <div className="border border-darkgrey mt-5"/>
                     {/* 본문  */}
-                    <p className="mt-10 font-['PretendardMedium'] text-base mb-24">{result.content}</p>
+                    <p className="mt-10 font-['PretendardMedium'] text-base mb-24">{detail.content}</p>
                     {/* 추천  및 스크랩*/}
                     <div className="flex flex-row gap-2.5 justify-center">
                         <CardLayoutLike
-                        _id={ result._id.toString()}
+                        _id={ detail._id.toString()}
                         type='like'
-                        author = {result.author.toString()} />
+                        author = {detail.author.toString()} />
                         <CardLayoutLike
-                        _id={ result._id.toString()}
-                        author = {result.author.toString()}
+                        _id={ detail._id.toString()}
+                        author = {detail.author.toString()}
                         type='scrap' />
                     </div>
                     {/* 목록 */}
@@ -80,7 +83,7 @@ export default async function Detail(props) {
                     {/* 댓글 */}
                     <p className="text-xl font-['Gsans'] mt-7">댓글</p>
                     {/* 댓글 입력창 */}
-                    <Comment _id={ result._id.toString()} />
+                    <Comment _id={ detail._id.toString()} />
                 </div>
             </div>
         </div>        
