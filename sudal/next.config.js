d20e7508f1318b647da5024d0eb849aacc095fb1
백민재 <baegminjae@baegminjaeui-MacBookPro.local>
2024-01-29 16:28:52 +0900

    feat : 본인 게시물만 delete

--- sudal/next.config.js
+++ sudal/next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['k.kakaocdn.net'],
-    },
-    // 여기에 다른 Next.js 구성 옵션을 추가할 수 있습니다.
-  }
-  
-  module.exports = nextConfig;
+  images: {
+    domains: ['k.kakaocdn.net'],
+    domains: ['ssl.pstatic.net'],
+  },
+  // 여기에 다른 Next.js 구성 옵션을 추가할 수 있습니다.
+}
+
+module.exports = nextConfig;
--- sudal/pages/api/posts/delete.js
+++ sudal/pages/api/posts/delete.js
@@ -4,18 +4,18 @@ import { ObjectId } from "mongodb";
 import { connectDB } from "../../../util/database";
 
 export default async function handler(req, res) {
-    if(req.method != 'DELETE') { return res.status(400).json('잘못된 접근입니다.') }
-    let session = await getServerSession(req, res, authOptions)
-    let column = req.query.column
-    const db = (await connectDB).db('posts')
-    let data = await db.collection(column).findOne({_id: new ObjectId(req.body)})
 
-    if(!session) { res.status(500).json('로그인이 필요합니다.') }
-    if(session.user.email != data.author) { res.status(500).json('작성자만 삭제할 수 있습니다.') }
+    if(req.method == 'DELETE') {
+        let session = await getServerSession(req, res, authOptions)
+        const db = (await connectDB).db('posts')
+        let data = await db.collection('post').findOne({_id: new ObjectId(req.body)})
+        if(data.author==session.user.email){
+            let result = await db.collection('post').deleteOne({_id: new ObjectId(req.body)})
+            return 응답.status(200).json('삭제완료')
+        }else {
+            return 응답.status(500).json('현재유저와 작성자 불일치')
+          }
 
-    if(!data) { return res.status(404).json('게시글이 존재하지 않습니다.') }
-    console.log(data);
-    await db.collection(column).deleteOne(data)
-
-    return res.status(200).json('게시글이 삭제되었습니다.')
+    }
+    
 }
