e8dffe5aefa784bc08d48eaec63ab50cc67b6bb9
HanJunSeo <hjs123@jbnu.ac.kr>
2024-01-29 16:47:49 +0900

    temp

--- sudal/pages/api/posts/delete.js
+++ sudal/pages/api/posts/delete.js
@@ -1,18 +1,18 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]";
 import { ObjectId } from "mongodb";
 import { connectDB } from "../../../util/database";
+import { authOptions } from "../auth/[...nextauth]"
 
 export default async function handler(req, res) {
     if(req.method != 'DELETE') { return res.status(400).json('잘못된 접근입니다.') }
-    let session = await getServerSession(req, res, authOptions)
     let column = req.query.column
+
+     let session = await getServerSession(req, res, authOptions)
+     if(!session) { res.status(400).json('로그인이 필요합니다.') }
+     if(session.user.id != data.author) { res.status(403).json('작성자만 삭제할 수 있습니다.') }
+
     const db = (await connectDB).db('posts')
     let data = await db.collection(column).findOne({_id: new ObjectId(req.body)})
-
-    if(!session) { return res.status(401).json('로그인이 필요합니다.') }
-    if(session.user.email != data.author) { return res.status(403).json('작성자만 삭제할 수 있습니다.') }
-
     if(!data) { return res.status(404).json('게시글이 존재하지 않습니다.') }
     console.log(data);
     await db.collection(column).deleteOne(data)
