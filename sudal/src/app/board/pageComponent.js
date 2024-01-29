9e640358d4c10e55a6320b57f82cea8c70ce3314
HanJunSeo <hjs123@jbnu.ac.kr>
2024-01-29 15:19:16 +0900

    fix: posting 수정
    posting 수정 중

--- sudal/src/app/board/pageComponent.js
+++ sudal/src/app/board/pageComponent.js
@@ -110,7 +110,7 @@ function Post({id, title, author, date, recommend, scrap, view, comment, board})
     <div className="flex flex-row items-center font-['PretendardMedium'] h-10 border-b">
       <div className="flex flex-row items-start w-1/2 ml-2">
         <Popular />
-        <Link prefetch={false} className="text-[18px] truncate mr-[3px]" href={`/detail/${id}?board=${board}`}>{title}</Link>
+        <Link prefetch={false} className="text-[18px] truncate mr-[3px]" href={`/detail/${id}`}>{title}</Link>
         <Comment comment={comment} />
       </div>
       <button className="text-center w-28 text-center text-[16px] truncate">{author}</button>
