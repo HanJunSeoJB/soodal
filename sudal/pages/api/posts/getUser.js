d0e307da4b1ab1986e990558d494751c3dafc0d7
HanJunSeoJB <hjs123@jbnu.ac.kr>
2024-02-20 16:41:49 +0900

    feat: 게시글 수정 시 로그인이 안되어있을 경우 메세지 표시

--- sudal/pages/api/posts/getUser.js
+++ sudal/pages/api/posts/getUser.js
@@ -7,8 +7,13 @@ export default async function handler(req, res) {
     if(req.method != 'POST') { return res.status(400).json('잘못된 접근입니다.') }
 
     let session = await getServerSession(req, res, authOptions)
+    if(!session) { return res.status(403).json('로그인이 필요합니다.') }
     const db = (await connectDB).db('posts')
-    let result = await db.collection('post').findOne({_id: new ObjectId(req.body), author: session.user.email})
+    let result = await db.collection('post')
+    .findOne({
+        _id: new ObjectId(req.body),
+        author: session.user.email
+    })
     if(result) {
         return res.status(200).json('작성자 일치')
     }
